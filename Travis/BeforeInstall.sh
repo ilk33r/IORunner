d82dfd56baf68870ea5e55b9e5ec691a57802218
ilker özcan <lkr_051@hotmail.com>
2016-08-21 01:21:12 +0300

    Travist - test.

--- Travis/BeforeInstall.sh
+++ Travis/BeforeInstall.sh
@@ -12,5 +12,5 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
 	curl https://swift.org/builds/swift-3.0-preview-4/ubuntu1404/swift-3.0-PREVIEW-4/swift-3.0-PREVIEW-4-ubuntu14.04.tar.gz -s | tar xz -C swift &> /dev/null
 
-	export PATH=$(pwd)/swift/$SWIFT_VERSION-ubuntu14.04/usr/bin:$PATH
+	env PATH=$(pwd)/swift/$SWIFT_VERSION-ubuntu14.04/usr/bin:$PATH
 fi
