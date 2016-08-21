bac0be380df2a69ff7118bfa2912696aa024efb5
ilker özcan <lkr_051@hotmail.com>
2016-08-21 10:10:45 +0300

    Travist - test.

--- Travis/Build.sh
+++ Travis/Build.sh
@@ -6,12 +6,9 @@
 #  Created by ilker özcan on 21/08/16.
 #
 
-echo $TRAVIS_OS_NAME
-
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
 	export PATH="$PWD/swift/$SWIFT_VERSION-ubuntu14.04/usr/bin:$PATH"
-	echo $PATH
 	which swiftc
 	make dist
 else
