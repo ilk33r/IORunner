bc597d7f8ec61501bb8a14776dd9a90e6ca42aa9
ilker özcan <lkr_051@hotmail.com>
2016-08-21 10:29:02 +0300

    Travist - test.

--- Travis/BeforeInstall.sh
+++ Travis/BeforeInstall.sh
@@ -17,4 +17,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	CLANG_DIR=$(dirname $CLANG_PATH)
 
 	sudo ln -sf /usr/lib/llvm-3.4/lib/LLVMgold.so $CLANG_DIR/../lib/LLVMgold.so
+
+	sudo apt-get -qq update
+	sudo apt-get install -y libbsd0
+	sudo apt-get install -y libbsd-dev
 fi
