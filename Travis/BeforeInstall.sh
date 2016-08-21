74c3d9d8773eab0c805221cd20db7fd8a47e3d24
ilker özcan <lkr_051@hotmail.com>
2016-08-21 10:23:02 +0300

    Travist - test.

--- Travis/BeforeInstall.sh
+++ Travis/BeforeInstall.sh
@@ -13,5 +13,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	curl https://swift.org/builds/swift-3.0-preview-4/ubuntu1404/swift-3.0-PREVIEW-4/swift-3.0-PREVIEW-4-ubuntu14.04.tar.gz -s | tar xz -C swift &> /dev/null
 	export PATH="$PWD/swift/$SWIFT_VERSION-ubuntu14.04/usr/bin:$PATH"
 
-	sudo ln -sf /usr/lib/llvm-3.4/lib/LLVMgold.so /usr/lib/LLVMgold.so
+	CLANG_PATH=$(which clang)
+	CLANG_DIR=$(dirname $CLANG_PATH)
+
+	sudo ln -sf /usr/lib/llvm-3.4/lib/LLVMgold.so $CLANG_DIR/../lib/LLVMgold.so
 fi
