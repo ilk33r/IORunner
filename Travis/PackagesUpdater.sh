42782542f6307b56066ce14fa6a0d00071e2e43a
ilker özcan <lkr_051@hotmail.com>
2016-09-06 17:42:29 +0300

    Working copy.

--- Travis/PackagesUpdater.sh
+++ Travis/PackagesUpdater.sh
@@ -9,20 +9,30 @@
 
 echo "Updating IOGUI ..."
 if [[ -d ./Packages/IOGUI ]]; then
+
 	git -C ./Packages/IOGUI reset --hard origin/master
 	git -C ./Packages/IOGUI pull
+
 else
+
 	git -C ./Packages clone https://github.com/ilk33r/IOGUI.git
+
 fi
 echo "[OK]"
 
+
 echo "Updating IOIni ..."
 if [[ -d ./Packages/IOIni ]]; then
+
 	git -C ./Packages/IOIni reset --hard origin/master
 	git -C ./Packages/IOIni pull
+
 else
+
 	git -C ./Packages clone https://github.com/ilk33r/IOIni.git
+
 fi
 echo "[OK]"
+
 
 exit 0
