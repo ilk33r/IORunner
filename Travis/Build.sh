74d216a65f74bf9a899a55c2691578c2a960186e
ilker özcan <lkr_051@hotmail.com>
2016-09-18 15:03:21 +0300

    Remove swift 2.2 compatibility.

--- Travis/Build.sh
+++ Travis/Build.sh
@@ -24,7 +24,27 @@ if [[ $TEST_BRANCH == $TRAVIS_BRANCH ]]; then
 		./Travis/PackagesUpdater.sh
 		make dist-clean
 		make dist
-		make deploy;
+		make deploy
+	fi
+
+elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+
+	if [[ $TRAVIS_IMAGE == 'Docker' ]]; then
+
+		if [[ $DOCKER_OS_RELEASE == 'Ubuntu_Trusty' ]]; then
+			SWIFT_PATH='/usr/swift/swift-3.0-PREVIEW-6-ubuntu14.04/usr/bin'
+		elif [[ $DOCKER_OS_RELEASE == 'Ubuntu_Wily' ]]; then
+			SWIFT_PATH='/usr/local/swift/swift-3.0-PREVIEW-6-ubuntu15.10/usr/bin'
+		else
+			SWIFT_PATH='/usr/local/swift/bin'
+		fi
+
+		docker run -e "OS_RELEASE=${DOCKER_OS_RELEASE}" -e "PATH=${SWIFT_PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" -e "TRAVIS_COMMIT=${TRAVIS_COMMIT}" -e "TRAVIS_IMAGE=${TRAVIS_IMAGE}" ilk3r/iorunner:${DOCKER_IMAGE} /bin/bash -c "cd /root/ilk33r/IORunner/; git reset --hard origin/master; git pull; ./Travis/PackagesUpdater.sh; make dist-clean; make dist;"
+
+	else
+		./Travis/PackagesUpdater.sh
+		make dist-clean
+		make dist
 	fi
 
 else
--- Travis/DownloadDocker.sh
+++ Travis/DownloadDocker.sh
@@ -19,6 +19,21 @@ if [[ $TEST_BRANCH == $TRAVIS_BRANCH ]]; then
 		docker run --name iorunner_${DOCKER_IMAGE} ilk3r/iorunner:${DOCKER_IMAGE} /bin/true
 		docker export -o docker/iorunner_${DOCKER_IMAGE}.tar iorunner_${DOCKER_IMAGE}
 	fi
+
+elif [[ $TRAVIS_BRANCH == 'master' ]]; then
+
+	if [[ ! -d docker ]]; then
+		mkdir docker
+	fi
+
+	if [[ -f docker/iorunner_${DOCKER_IMAGE}.tar ]]; then
+		docker import docker/iorunner_${DOCKER_IMAGE}.tar ilk3r/iorunner:${DOCKER_IMAGE}
+	else
+		docker pull ilk3r/iorunner:${DOCKER_IMAGE}
+		docker run --name iorunner_${DOCKER_IMAGE} ilk3r/iorunner:${DOCKER_IMAGE} /bin/true
+		docker export -o docker/iorunner_${DOCKER_IMAGE}.tar iorunner_${DOCKER_IMAGE}
+	fi
+
 else
 	exit 0
 fi
